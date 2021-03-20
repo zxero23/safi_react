b892a08f712d8212f5ae98f0f3d3b02d7df73121
facu <facundo.gonzalez@marandu.com.ar>
2021-03-20 17:56:35 -0300

    denuncias

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,13 +2,15 @@ import React  from 'react';
 import {BrowserRouter,Switch,Route} from 'react-router-dom';
 import Login from '../pages/Login';
 import Menu from '../pages/Menu';
+import Mapa from '../pages/MapView';
 
 function routes() {
   return (
     <BrowserRouter>
         <Switch>
             <Route exact path='/' component={Login} />   
-            <Route exact path="/menu" component={Menu}/>         
+            <Route exact path="/menu" component={Menu}/>  
+            <Route exact path="/mapa" component={Mapa}/>       
         </Switch>
     </BrowserRouter>
   );
